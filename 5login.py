86906c44f547fad8ef10633f6854f01ea82ba8bc
ANJI1196 <76936911+ANJI1196@users.noreply.github.com>
2021-01-06 17:57:32 +0530

    Add files via upload

---
+++ 5login.py
@@ -0,0 +1,8 @@
+#Login App:
+username=input("Please Enter Valid USer Name:")
+password=input("Please Enter Valid Password:")
+if username=='anji' and password=='anji1234':
+    print("Your User Name:",username)
+else:
+    print("Entered Wrong password")
+    print("BYE")
